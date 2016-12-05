d0493b2f5498a4defca2486a11009c8f94a21fc6
Isaac <isaacmgoldner@gmail.com>
2016-12-05 10:56:28 -0500

    Added MoveNoGyro left method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveNoGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveNoGyro.java
@@ -70,4 +70,49 @@ public class MoveNoGyro extends move {
         sleep(50);
     }
 
+    public static void left(double distance, double power) throws InterruptedException {
+        flmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        blmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
+        double ROTATIONS = distance / CIRCUMFERENCE;
+        double COUNTS = ENCODER_CPR * ROTATIONS * GEAR_RATIO;
+
+        flmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        blmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        flmotor.setTargetPosition((int) COUNTS);
+        frmotor.setTargetPosition((int) COUNTS);
+        blmotor.setTargetPosition((int) COUNTS);
+        brmotor.setTargetPosition((int) COUNTS);
+
+        flmotor.setPower(power);
+        frmotor.setPower(power);
+        blmotor.setPower(power);
+        brmotor.setPower(power);
+
+        while (frmotor.getCurrentPosition() < COUNTS) {
+            flmotor.setPower(Math.min((Math.pow(-COUNTS + flmotor.getCurrentPosition(), 2))/500, 0.5));
+            frmotor.setPower(Math.min((Math.pow(COUNTS - frmotor.getCurrentPosition(), 2))/500, 0.5));
+            blmotor.setPower(Math.min((Math.pow(COUNTS - blmotor.getCurrentPosition(), 2))/500, 0.5));
+            brmotor.setPower(Math.min((Math.pow(-COUNTS + brmotor.getCurrentPosition(), 2))/500, 0.5));
+        }
+
+        flmotor.setPower(0);
+        frmotor.setPower(0);
+        blmotor.setPower(0);
+        brmotor.setPower(0);
+
+        flmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        blmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        sleep(50);
+    }
+
 }
