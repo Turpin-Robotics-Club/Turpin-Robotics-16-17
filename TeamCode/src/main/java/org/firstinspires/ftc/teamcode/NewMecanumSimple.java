c999d91e1b56ea64ecab871d9fdeb729b4b97dc1
Podshot <ben.gothard3@gmail.com>
2016-12-16 17:33:18 -0500

    Added storage servo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewMecanumSimple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewMecanumSimple.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by Jonathan on 12/16/2016.
@@ -17,7 +19,18 @@ public class NewMecanumSimple extends OpMode {
     DcMotor backleft;
     DcMotor backright;
 
+    DcMotor leftShooter;
+    DcMotor rightShooter;
+
+    Servo storageServo;
+
     public final double SPEED = 0.75;
+
+    boolean shooterRunning = false;
+
+    private ElapsedTime runtime_b = new ElapsedTime();
+    private ElapsedTime runtimeStorageServo = new ElapsedTime();
+    private ElapsedTime runtine_y = new ElapsedTime();
 
     @Override
     public void init() {
@@ -26,8 +39,17 @@ public class NewMecanumSimple extends OpMode {
         backleft = hardwareMap.dcMotor.get("back_left");
         backright = hardwareMap.dcMotor.get("back_right");
 
+        leftShooter = hardwareMap.dcMotor.get("left_shooter");
+        rightShooter = hardwareMap.dcMotor.get("right_shooter");
+
+        storageServo = hardwareMap.servo.get("storage_servo");
+
         backleft.setDirection(DcMotorSimple.Direction.REVERSE);
         backright.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        runtime_b.reset();
+        runtine_y.reset();
+        runtimeStorageServo.reset();
     }
 
     @Override
@@ -54,6 +76,36 @@ public class NewMecanumSimple extends OpMode {
         frontright.setPower(frontRightPower * SPEED);
         backleft.setPower(backLeftPower * SPEED);
         backright.setPower(backRightPower * SPEED);
+
+        if (shooterRunning) {
+            if (leftShooter.getPower() < 0.2) {
+                leftShooter.setPower(0.2);
+                rightShooter.setPower(-0.2);
+            }
+            double i = Math.min(leftShooter.getPower() + 0.05, 0.7);
+            leftShooter.setPower(i);
+            rightShooter.setPower(-i);
+        } else {
+            double i = Math.max(leftShooter.getPower() - 0.05, 0.0);
+            leftShooter.setPower(i);
+            rightShooter.setPower(-i);
+        }
+
+        if (runtimeStorageServo.seconds() >= 1 && (storageServo.getPosition() == 0)) {
+            storageServo.setPosition(1);
+        }
+
+        if (gamepad2.b && (runtime_b.seconds() >= 2)) {
+            shooterRunning = !shooterRunning;
+            runtime_b.reset();
+        }
+
+        if (gamepad2.y && (runtime_b.seconds() >= 2)) {
+            storageServo.setPosition(0);
+            runtimeStorageServo.reset();
+        }
+
+
     }
 
     @Override
