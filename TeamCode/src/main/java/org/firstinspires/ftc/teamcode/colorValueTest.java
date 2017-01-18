dfb98058e261a9375c3cec3905f0163a38400e5a
Podshot <ben.gothard3@gmail.com>
2017-01-18 11:02:18 -0500

    Added more info to colorValueTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorValueTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorValueTest.java
@@ -2,8 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.teamcode.utils.Sensors;
+
+import java.util.Iterator;
 
 
 @TeleOp(name="Color Value Test", group="Utilities")
@@ -17,8 +20,19 @@ public class colorValueTest extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addData("red value" ,Sensors.leye.red());
-        telemetry.addData("blue value", Sensors.leye.blue());
+        telemetry.addData("left red value" ,Sensors.leye.red());
+        telemetry.addData("left blue value", Sensors.leye.blue());
+
+        telemetry.addData("right red value", Sensors.reye.red());
+        telemetry.addData("right blue value", Sensors.reye.blue());
+
+        Iterator<ColorSensor> iter = hardwareMap.colorSensor.iterator();
+        while (iter.hasNext()) {
+            ColorSensor sensor = iter.next();
+            telemetry.addData("Color I2C Address", Integer.toHexString(sensor.getI2cAddress().get8Bit()));
+        }
+        telemetry.update();
+
 
 
     }
